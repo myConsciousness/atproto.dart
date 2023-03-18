742f2623bae8ee859d2dbf29c016423fa984cd70
myConsciousness <contact@shinyakato.dev>
2023-03-18 10:10:52 +0900

    feat: add `xrpc` package

---
+++ packages/xrpc/test/xrpc_test.dart
@@ -0,0 +1,13 @@
+// Copyright 2023 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    test('First Test', () {
+      //
+    });
+  });
+}
