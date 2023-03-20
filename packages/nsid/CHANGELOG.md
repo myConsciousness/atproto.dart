48f5c052addbc0234a4f5a8614b743ecf399f1f9
myConsciousness <contact@shinyakato.dev>
2023-03-20 13:17:32 +0900

    docs: fixed dartdoc

--- packages/nsid/CHANGELOG.md
+++ packages/nsid/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release Note
 
+## v0.0.2
+
+- Refactored dartdoc.
+
 ## v0.0.1
 
 - First Release.
--- packages/nsid/lib/src/nsid.dart
+++ packages/nsid/lib/src/nsid.dart
@@ -8,14 +8,14 @@ import 'invalid_nsid_error.dart';
 ///
 /// # Grammar:
 ///
-/// alpha     = "a" / "b" / "c" / "d" / "e" / "f" / "g" / "h" / "i" / "j" / "k" / "l" / "m" / "n" / "o" / "p" / "q" / "r" / "s" / "t" / "u" / "v" / "w" / "x" / "y" / "z" / "A" / "B" / "C" / "D" / "E" / "F" / "G" / "H" / "I" / "J" / "K" / "L" / "M" / "N" / "O" / "P" / "Q" / "R" / "S" / "T" / "U" / "V" / "W" / "X" / "Y" / "Z"
-/// number    = "1" / "2" / "3" / "4" / "5" / "6" / "7" / "8" / "9" / "0"
-/// delim     = "."
-/// segment   = alpha *( alpha / number / "-" )
-/// authority = segment *( delim segment )
-/// name      = segment
-/// nsid      = authority delim name
-/// nsid-ns   = authority delim "*"
+/// - alpha     = "a" / "b" / "c" / "d" / "e" / "f" / "g" / "h" / "i" / "j" / "k" / "l" / "m" / "n" / "o" / "p" / "q" / "r" / "s" / "t" / "u" / "v" / "w" / "x" / "y" / "z" / "A" / "B" / "C" / "D" / "E" / "F" / "G" / "H" / "I" / "J" / "K" / "L" / "M" / "N" / "O" / "P" / "Q" / "R" / "S" / "T" / "U" / "V" / "W" / "X" / "Y" / "Z"
+/// - number    = "1" / "2" / "3" / "4" / "5" / "6" / "7" / "8" / "9" / "0"
+/// - delim     = "."
+/// - segment   = alpha *( alpha / number / "-" )
+/// - authority = segment *( delim segment )
+/// - name      = segment
+/// - nsid      = authority delim name
+/// - nsid-ns   = authority delim "*"
 class NSID {
   factory NSID.parse(final String nsid) => NSID._(nsid);
 
