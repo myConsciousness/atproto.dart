52826b054608ece28671d021831fae80d3cba90c
myConsciousness <contact@shinyakato.dev>
2023-03-22 15:31:26 +0900

    feat: exposed `To` typedef

--- packages/xrpc/lib/xrpc.dart
+++ packages/xrpc/lib/xrpc.dart
@@ -13,7 +13,7 @@ export 'package:xrpc/src/exception/xrpc_exception.dart';
 export 'package:xrpc/src/http_method.dart';
 export 'package:xrpc/src/http_status.dart';
 export 'package:xrpc/src/serializable.dart';
-export 'package:xrpc/src/xrpc.dart' show query, procedure;
+export 'package:xrpc/src/xrpc.dart' show query, procedure, To;
 export 'package:xrpc/src/xrpc_error.dart';
 export 'package:xrpc/src/xrpc_request.dart';
 export 'package:xrpc/src/xrpc_response.dart';
