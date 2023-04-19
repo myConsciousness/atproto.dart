15ae22fa88f1a494f9d426b52c54dba1808619a4
myConsciousness <contact@shinyakato.dev>
2023-04-19 16:39:00 +0900

    feat: supported `protocol` options

---
+++ packages/xrpc/lib/src/exception/xrpc_not_supported_exception.dart
@@ -0,0 +1,12 @@
+// Copyright 2023 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+// 🌎 Project imports:
+import 'xrpc_exception.dart';
+
+class XRPCNotSupportedException extends XRPCException {
+  /// Returns the new instance of [XRPCNotSupportedException].
+  const XRPCNotSupportedException(super.response)
+      : super(label: 'XRPCNotSupportedException');
+}
