20b1acc3648db9aac030b4d40cfb5c4fc6b5dc55
myConsciousness <contact@shinyakato.dev>
2023-05-08 10:05:16 +0900

    fix: fixed copyright

--- packages/bluesky_text/lib/src/entities/byte_indices.dart
+++ packages/bluesky_text/lib/src/entities/byte_indices.dart
@@ -2,10 +2,6 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
-// Copyright 2023 Shinya Kato. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
-
 // ignore_for_file: invalid_annotation_target
 
 // 📦 Package imports:
