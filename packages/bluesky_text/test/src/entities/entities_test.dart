4c3acf1ae389ce0bee7f2c25c4d315d66203a472
myConsciousness <contact@shinyakato.dev>
2023-05-10 10:54:18 +0900

    fix: fixed for the issue (#354)

---
+++ packages/bluesky_text/test/src/entities/entities_test.dart
@@ -0,0 +1,50 @@
+// Copyright 2023 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+import 'package:bluesky_text/src/entities/byte_indices.dart';
+import 'package:bluesky_text/src/entities/entities.dart';
+import 'package:bluesky_text/src/entities/entity.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('.toFacets', () {
+    test('case1', () async {
+      final entities = Entities([
+        Entity(
+          type: EntityType.handle,
+          value: '@shinyakato.dev',
+          indices: ByteIndices(start: 0, end: 0),
+        )
+      ]);
+
+      final facets = await entities.toFacets();
+
+      expect(facets, [
+        {
+          'index': {'byteStart': 0, 'byteEnd': 0},
+          'features': [
+            {
+              '\$type': 'app.bsky.richtext.facet#mention',
+              'did': 'did:plc:iijrtk7ocored6zuziwmqq3c'
+            }
+          ]
+        }
+      ]);
+    });
+
+    test('case2', () async {
+      final entities = Entities([
+        Entity(
+          type: EntityType.handle,
+          value: 'a.bsky.social',
+          indices: ByteIndices(start: 0, end: 0),
+        )
+      ]);
+
+      final facets = await entities.toFacets();
+
+      expect(facets, []);
+    });
+  });
+}
