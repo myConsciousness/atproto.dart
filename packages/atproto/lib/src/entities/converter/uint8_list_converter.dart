ffe889059b957e9a7c7a6a73a94d58264732c5b7
myConsciousness <contact@shinyakato.dev>
2023-05-30 15:02:18 +0900

    fix

--- packages/atproto/lib/src/entities/converter/uint8_list_converter.dart
+++ packages/atproto/lib/src/entities/converter/uint8_list_converter.dart
@@ -19,5 +19,5 @@ class Uint8ListConverter implements JsonConverter<Uint8List?, dynamic> {
   }
 
   @override
-  dynamic toJson(Uint8List? object) => object?.toString();
+  dynamic toJson(Uint8List? object) => object?.toList();
 }
