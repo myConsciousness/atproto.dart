4862eff46cfa010092fe151c84ff20db40bab6c0
myConsciousness <myConsciousness@users.noreply.github.com>
2023-07-25 10:02:45 +0000

    [Scheduled] Auto Mirror Lexicons

--- packages/bluesky/lib/src/entities/keys/ids.g.dart
+++ packages/bluesky/lib/src/entities/keys/ids.g.dart
@@ -13,6 +13,48 @@
 /// `blob`
 const blob = 'blob';
 
+/// `app.bsky.notification.updateSeen`
+const appBskyNotificationUpdateSeen = 'app.bsky.notification.updateSeen';
+
+/// `app.bsky.notification.getUnreadCount`
+const appBskyNotificationGetUnreadCount = 'app.bsky.notification.getUnreadCount';
+
+/// `app.bsky.notification.listNotifications`
+const appBskyNotificationListNotifications = 'app.bsky.notification.listNotifications';
+
+/// `app.bsky.notification.listNotifications#notification`
+const appBskyNotificationListNotificationsNotification = 'app.bsky.notification.listNotifications#notification';
+
+/// `app.bsky.richtext.facet`
+const appBskyRichtextFacet = 'app.bsky.richtext.facet';
+
+/// `app.bsky.richtext.facet#mention`
+const appBskyRichtextFacetMention = 'app.bsky.richtext.facet#mention';
+
+/// `app.bsky.richtext.facet#link`
+const appBskyRichtextFacetLink = 'app.bsky.richtext.facet#link';
+
+/// `app.bsky.richtext.facet#byteSlice`
+const appBskyRichtextFacetByteSlice = 'app.bsky.richtext.facet#byteSlice';
+
+/// `app.bsky.embed.recordWithMedia`
+const appBskyEmbedRecordWithMedia = 'app.bsky.embed.recordWithMedia';
+
+/// `app.bsky.embed.recordWithMedia#view`
+const appBskyEmbedRecordWithMediaView = 'app.bsky.embed.recordWithMedia#view';
+
+/// `app.bsky.embed.external`
+const appBskyEmbedExternal = 'app.bsky.embed.external';
+
+/// `app.bsky.embed.external#external`
+const appBskyEmbedExternalExternal = 'app.bsky.embed.external#external';
+
+/// `app.bsky.embed.external#view`
+const appBskyEmbedExternalView = 'app.bsky.embed.external#view';
+
+/// `app.bsky.embed.external#viewExternal`
+const appBskyEmbedExternalViewExternal = 'app.bsky.embed.external#viewExternal';
+
 /// `app.bsky.embed.record`
 const appBskyEmbedRecord = 'app.bsky.embed.record';
 
@@ -40,55 +82,71 @@ const appBskyEmbedImagesView = 'app.bsky.embed.images#view';
 /// `app.bsky.embed.images#viewImage`
 const appBskyEmbedImagesViewImage = 'app.bsky.embed.images#viewImage';
 
-/// `app.bsky.embed.recordWithMedia`
-const appBskyEmbedRecordWithMedia = 'app.bsky.embed.recordWithMedia';
+/// `app.bsky.actor.searchActorsTypeahead`
+const appBskyActorSearchActorsTypeahead = 'app.bsky.actor.searchActorsTypeahead';
 
-/// `app.bsky.embed.recordWithMedia#view`
-const appBskyEmbedRecordWithMediaView = 'app.bsky.embed.recordWithMedia#view';
+/// `app.bsky.actor.getProfiles`
+const appBskyActorGetProfiles = 'app.bsky.actor.getProfiles';
 
-/// `app.bsky.embed.external`
-const appBskyEmbedExternal = 'app.bsky.embed.external';
+/// `app.bsky.actor.getPreferences`
+const appBskyActorGetPreferences = 'app.bsky.actor.getPreferences';
 
-/// `app.bsky.embed.external#external`
-const appBskyEmbedExternalExternal = 'app.bsky.embed.external#external';
+/// `app.bsky.actor.getSuggestions`
+const appBskyActorGetSuggestions = 'app.bsky.actor.getSuggestions';
 
-/// `app.bsky.embed.external#view`
-const appBskyEmbedExternalView = 'app.bsky.embed.external#view';
+/// `app.bsky.actor.defs`
+const appBskyActorDefs = 'app.bsky.actor.defs';
 
-/// `app.bsky.embed.external#viewExternal`
-const appBskyEmbedExternalViewExternal = 'app.bsky.embed.external#viewExternal';
+/// `app.bsky.actor.defs#profileViewBasic`
+const appBskyActorDefsProfileViewBasic = 'app.bsky.actor.defs#profileViewBasic';
 
-/// `app.bsky.notification.updateSeen`
-const appBskyNotificationUpdateSeen = 'app.bsky.notification.updateSeen';
+/// `app.bsky.actor.defs#profileView`
+const appBskyActorDefsProfileView = 'app.bsky.actor.defs#profileView';
 
-/// `app.bsky.notification.listNotifications`
-const appBskyNotificationListNotifications =
-    'app.bsky.notification.listNotifications';
+/// `app.bsky.actor.defs#profileViewDetailed`
+const appBskyActorDefsProfileViewDetailed = 'app.bsky.actor.defs#profileViewDetailed';
 
-/// `app.bsky.notification.listNotifications#notification`
-const appBskyNotificationListNotificationsNotification =
-    'app.bsky.notification.listNotifications#notification';
+/// `app.bsky.actor.defs#viewerState`
+const appBskyActorDefsViewerState = 'app.bsky.actor.defs#viewerState';
 
-/// `app.bsky.notification.getUnreadCount`
-const appBskyNotificationGetUnreadCount =
-    'app.bsky.notification.getUnreadCount';
+/// `app.bsky.actor.defs#preferences`
+const appBskyActorDefsPreferences = 'app.bsky.actor.defs#preferences';
+
+/// `app.bsky.actor.defs#adultContentPref`
+const appBskyActorDefsAdultContentPref = 'app.bsky.actor.defs#adultContentPref';
+
+/// `app.bsky.actor.defs#contentLabelPref`
+const appBskyActorDefsContentLabelPref = 'app.bsky.actor.defs#contentLabelPref';
+
+/// `app.bsky.actor.defs#savedFeedsPref`
+const appBskyActorDefsSavedFeedsPref = 'app.bsky.actor.defs#savedFeedsPref';
+
+/// `app.bsky.actor.getProfile`
+const appBskyActorGetProfile = 'app.bsky.actor.getProfile';
+
+/// `app.bsky.actor.searchActors`
+const appBskyActorSearchActors = 'app.bsky.actor.searchActors';
+
+/// `app.bsky.actor.profile`
+const appBskyActorProfile = 'app.bsky.actor.profile';
+
+/// `app.bsky.actor.putPreferences`
+const appBskyActorPutPreferences = 'app.bsky.actor.putPreferences';
+
+/// `app.bsky.unspecced.getPopularFeedGenerators`
+const appBskyUnspeccedGetPopularFeedGenerators = 'app.bsky.unspecced.getPopularFeedGenerators';
+
+/// `app.bsky.unspecced.getTimelineSkeleton`
+const appBskyUnspeccedGetTimelineSkeleton = 'app.bsky.unspecced.getTimelineSkeleton';
 
 /// `app.bsky.unspecced.getPopular`
 const appBskyUnspeccedGetPopular = 'app.bsky.unspecced.getPopular';
 
-/// `app.bsky.unspecced.getPopularFeedGenerators`
-const appBskyUnspeccedGetPopularFeedGenerators =
-    'app.bsky.unspecced.getPopularFeedGenerators';
+/// `app.bsky.graph.muteActor`
+const appBskyGraphMuteActor = 'app.bsky.graph.muteActor';
 
-/// `app.bsky.unspecced.getTimelineSkeleton`
-const appBskyUnspeccedGetTimelineSkeleton =
-    'app.bsky.unspecced.getTimelineSkeleton';
-
-/// `app.bsky.graph.block`
-const appBskyGraphBlock = 'app.bsky.graph.block';
-
-/// `app.bsky.graph.follow`
-const appBskyGraphFollow = 'app.bsky.graph.follow';
+/// `app.bsky.graph.getMutes`
+const appBskyGraphGetMutes = 'app.bsky.graph.getMutes';
 
 /// `app.bsky.graph.defs`
 const appBskyGraphDefs = 'app.bsky.graph.defs';
@@ -111,11 +169,20 @@ const appBskyGraphDefsModlist = 'app.bsky.graph.defs#modlist';
 /// `app.bsky.graph.defs#listViewerState`
 const appBskyGraphDefsListViewerState = 'app.bsky.graph.defs#listViewerState';
 
-/// `app.bsky.graph.unmuteActorList`
-const appBskyGraphUnmuteActorList = 'app.bsky.graph.unmuteActorList';
+/// `app.bsky.graph.getBlocks`
+const appBskyGraphGetBlocks = 'app.bsky.graph.getBlocks';
 
 /// `app.bsky.graph.muteActorList`
 const appBskyGraphMuteActorList = 'app.bsky.graph.muteActorList';
+
+/// `app.bsky.graph.unmuteActor`
+const appBskyGraphUnmuteActor = 'app.bsky.graph.unmuteActor';
+
+/// `app.bsky.graph.getFollows`
+const appBskyGraphGetFollows = 'app.bsky.graph.getFollows';
+
+/// `app.bsky.graph.block`
+const appBskyGraphBlock = 'app.bsky.graph.block';
 
 /// `app.bsky.graph.getLists`
 const appBskyGraphGetLists = 'app.bsky.graph.getLists';
@@ -123,14 +190,11 @@ const appBskyGraphGetLists = 'app.bsky.graph.getLists';
 /// `app.bsky.graph.getFollowers`
 const appBskyGraphGetFollowers = 'app.bsky.graph.getFollowers';
 
-/// `app.bsky.graph.muteActor`
-const appBskyGraphMuteActor = 'app.bsky.graph.muteActor';
+/// `app.bsky.graph.follow`
+const appBskyGraphFollow = 'app.bsky.graph.follow';
 
-/// `app.bsky.graph.getMutes`
-const appBskyGraphGetMutes = 'app.bsky.graph.getMutes';
-
-/// `app.bsky.graph.listitem`
-const appBskyGraphListitem = 'app.bsky.graph.listitem';
+/// `app.bsky.graph.unmuteActorList`
+const appBskyGraphUnmuteActorList = 'app.bsky.graph.unmuteActorList';
 
 /// `app.bsky.graph.list`
 const appBskyGraphList = 'app.bsky.graph.list';
@@ -138,20 +202,44 @@ const appBskyGraphList = 'app.bsky.graph.list';
 /// `app.bsky.graph.getListMutes`
 const appBskyGraphGetListMutes = 'app.bsky.graph.getListMutes';
 
-/// `app.bsky.graph.getFollows`
-const appBskyGraphGetFollows = 'app.bsky.graph.getFollows';
-
-/// `app.bsky.graph.getBlocks`
-const appBskyGraphGetBlocks = 'app.bsky.graph.getBlocks';
-
-/// `app.bsky.graph.unmuteActor`
-const appBskyGraphUnmuteActor = 'app.bsky.graph.unmuteActor';
-
 /// `app.bsky.graph.getList`
 const appBskyGraphGetList = 'app.bsky.graph.getList';
 
-/// `app.bsky.feed.generator`
-const appBskyFeedGenerator = 'app.bsky.feed.generator';
+/// `app.bsky.graph.listitem`
+const appBskyGraphListitem = 'app.bsky.graph.listitem';
+
+/// `app.bsky.feed.getActorFeeds`
+const appBskyFeedGetActorFeeds = 'app.bsky.feed.getActorFeeds';
+
+/// `app.bsky.feed.post`
+const appBskyFeedPost = 'app.bsky.feed.post';
+
+/// `app.bsky.feed.post#replyRef`
+const appBskyFeedPostReplyRef = 'app.bsky.feed.post#replyRef';
+
+/// `app.bsky.feed.post#entity`
+const appBskyFeedPostEntity = 'app.bsky.feed.post#entity';
+
+/// `app.bsky.feed.post#textSlice`
+const appBskyFeedPostTextSlice = 'app.bsky.feed.post#textSlice';
+
+/// `app.bsky.feed.getLikes`
+const appBskyFeedGetLikes = 'app.bsky.feed.getLikes';
+
+/// `app.bsky.feed.getLikes#like`
+const appBskyFeedGetLikesLike = 'app.bsky.feed.getLikes#like';
+
+/// `app.bsky.feed.getAuthorFeed`
+const appBskyFeedGetAuthorFeed = 'app.bsky.feed.getAuthorFeed';
+
+/// `app.bsky.feed.getPosts`
+const appBskyFeedGetPosts = 'app.bsky.feed.getPosts';
+
+/// `app.bsky.feed.like`
+const appBskyFeedLike = 'app.bsky.feed.like';
+
+/// `app.bsky.feed.repost`
+const appBskyFeedRepost = 'app.bsky.feed.repost';
 
 /// `app.bsky.feed.defs`
 const appBskyFeedDefs = 'app.bsky.feed.defs';
@@ -184,142 +272,43 @@ const appBskyFeedDefsBlockedPost = 'app.bsky.feed.defs#blockedPost';
 const appBskyFeedDefsGeneratorView = 'app.bsky.feed.defs#generatorView';
 
 /// `app.bsky.feed.defs#generatorViewerState`
-const appBskyFeedDefsGeneratorViewerState =
-    'app.bsky.feed.defs#generatorViewerState';
+const appBskyFeedDefsGeneratorViewerState = 'app.bsky.feed.defs#generatorViewerState';
 
 /// `app.bsky.feed.defs#skeletonFeedPost`
 const appBskyFeedDefsSkeletonFeedPost = 'app.bsky.feed.defs#skeletonFeedPost';
 
 /// `app.bsky.feed.defs#skeletonReasonRepost`
-const appBskyFeedDefsSkeletonReasonRepost =
-    'app.bsky.feed.defs#skeletonReasonRepost';
-
-/// `app.bsky.feed.getFeedGenerators`
-const appBskyFeedGetFeedGenerators = 'app.bsky.feed.getFeedGenerators';
+const appBskyFeedDefsSkeletonReasonRepost = 'app.bsky.feed.defs#skeletonReasonRepost';
 
 /// `app.bsky.feed.getTimeline`
 const appBskyFeedGetTimeline = 'app.bsky.feed.getTimeline';
 
-/// `app.bsky.feed.getFeedGenerator`
-const appBskyFeedGetFeedGenerator = 'app.bsky.feed.getFeedGenerator';
+/// `app.bsky.feed.getFeed`
+const appBskyFeedGetFeed = 'app.bsky.feed.getFeed';
 
-/// `app.bsky.feed.getAuthorFeed`
-const appBskyFeedGetAuthorFeed = 'app.bsky.feed.getAuthorFeed';
+/// `app.bsky.feed.getFeedGenerators`
+const appBskyFeedGetFeedGenerators = 'app.bsky.feed.getFeedGenerators';
 
-/// `app.bsky.feed.getLikes`
-const appBskyFeedGetLikes = 'app.bsky.feed.getLikes';
-
-/// `app.bsky.feed.getLikes#like`
-const appBskyFeedGetLikesLike = 'app.bsky.feed.getLikes#like';
-
-/// `app.bsky.feed.getPostThread`
-const appBskyFeedGetPostThread = 'app.bsky.feed.getPostThread';
-
-/// `app.bsky.feed.like`
-const appBskyFeedLike = 'app.bsky.feed.like';
+/// `app.bsky.feed.getFeedSkeleton`
+const appBskyFeedGetFeedSkeleton = 'app.bsky.feed.getFeedSkeleton';
 
 /// `app.bsky.feed.getRepostedBy`
 const appBskyFeedGetRepostedBy = 'app.bsky.feed.getRepostedBy';
 
-/// `app.bsky.feed.repost`
-const appBskyFeedRepost = 'app.bsky.feed.repost';
+/// `app.bsky.feed.getPostThread`
+const appBskyFeedGetPostThread = 'app.bsky.feed.getPostThread';
+
+/// `app.bsky.feed.generator`
+const appBskyFeedGenerator = 'app.bsky.feed.generator';
 
 /// `app.bsky.feed.describeFeedGenerator`
 const appBskyFeedDescribeFeedGenerator = 'app.bsky.feed.describeFeedGenerator';
 
 /// `app.bsky.feed.describeFeedGenerator#feed`
-const appBskyFeedDescribeFeedGeneratorFeed =
-    'app.bsky.feed.describeFeedGenerator#feed';
+const appBskyFeedDescribeFeedGeneratorFeed = 'app.bsky.feed.describeFeedGenerator#feed';
 
 /// `app.bsky.feed.describeFeedGenerator#links`
-const appBskyFeedDescribeFeedGeneratorLinks =
-    'app.bsky.feed.describeFeedGenerator#links';
+const appBskyFeedDescribeFeedGeneratorLinks = 'app.bsky.feed.describeFeedGenerator#links';
 
-/// `app.bsky.feed.getPosts`
-const appBskyFeedGetPosts = 'app.bsky.feed.getPosts';
-
-/// `app.bsky.feed.getFeed`
-const appBskyFeedGetFeed = 'app.bsky.feed.getFeed';
-
-/// `app.bsky.feed.getFeedSkeleton`
-const appBskyFeedGetFeedSkeleton = 'app.bsky.feed.getFeedSkeleton';
-
-/// `app.bsky.feed.getActorFeeds`
-const appBskyFeedGetActorFeeds = 'app.bsky.feed.getActorFeeds';
-
-/// `app.bsky.feed.post`
-const appBskyFeedPost = 'app.bsky.feed.post';
-
-/// `app.bsky.feed.post#replyRef`
-const appBskyFeedPostReplyRef = 'app.bsky.feed.post#replyRef';
-
-/// `app.bsky.feed.post#entity`
-const appBskyFeedPostEntity = 'app.bsky.feed.post#entity';
-
-/// `app.bsky.feed.post#textSlice`
-const appBskyFeedPostTextSlice = 'app.bsky.feed.post#textSlice';
-
-/// `app.bsky.richtext.facet`
-const appBskyRichtextFacet = 'app.bsky.richtext.facet';
-
-/// `app.bsky.richtext.facet#mention`
-const appBskyRichtextFacetMention = 'app.bsky.richtext.facet#mention';
-
-/// `app.bsky.richtext.facet#link`
-const appBskyRichtextFacetLink = 'app.bsky.richtext.facet#link';
-
-/// `app.bsky.richtext.facet#byteSlice`
-const appBskyRichtextFacetByteSlice = 'app.bsky.richtext.facet#byteSlice';
-
-/// `app.bsky.actor.searchActorsTypeahead`
-const appBskyActorSearchActorsTypeahead =
-    'app.bsky.actor.searchActorsTypeahead';
-
-/// `app.bsky.actor.defs`
-const appBskyActorDefs = 'app.bsky.actor.defs';
-
-/// `app.bsky.actor.defs#profileViewBasic`
-const appBskyActorDefsProfileViewBasic = 'app.bsky.actor.defs#profileViewBasic';
-
-/// `app.bsky.actor.defs#profileView`
-const appBskyActorDefsProfileView = 'app.bsky.actor.defs#profileView';
-
-/// `app.bsky.actor.defs#profileViewDetailed`
-const appBskyActorDefsProfileViewDetailed =
-    'app.bsky.actor.defs#profileViewDetailed';
-
-/// `app.bsky.actor.defs#viewerState`
-const appBskyActorDefsViewerState = 'app.bsky.actor.defs#viewerState';
-
-/// `app.bsky.actor.defs#preferences`
-const appBskyActorDefsPreferences = 'app.bsky.actor.defs#preferences';
-
-/// `app.bsky.actor.defs#adultContentPref`
-const appBskyActorDefsAdultContentPref = 'app.bsky.actor.defs#adultContentPref';
-
-/// `app.bsky.actor.defs#contentLabelPref`
-const appBskyActorDefsContentLabelPref = 'app.bsky.actor.defs#contentLabelPref';
-
-/// `app.bsky.actor.defs#savedFeedsPref`
-const appBskyActorDefsSavedFeedsPref = 'app.bsky.actor.defs#savedFeedsPref';
-
-/// `app.bsky.actor.putPreferences`
-const appBskyActorPutPreferences = 'app.bsky.actor.putPreferences';
-
-/// `app.bsky.actor.getProfile`
-const appBskyActorGetProfile = 'app.bsky.actor.getProfile';
-
-/// `app.bsky.actor.getSuggestions`
-const appBskyActorGetSuggestions = 'app.bsky.actor.getSuggestions';
-
-/// `app.bsky.actor.searchActors`
-const appBskyActorSearchActors = 'app.bsky.actor.searchActors';
-
-/// `app.bsky.actor.getProfiles`
-const appBskyActorGetProfiles = 'app.bsky.actor.getProfiles';
-
-/// `app.bsky.actor.getPreferences`
-const appBskyActorGetPreferences = 'app.bsky.actor.getPreferences';
-
-/// `app.bsky.actor.profile`
-const appBskyActorProfile = 'app.bsky.actor.profile';
+/// `app.bsky.feed.getFeedGenerator`
+const appBskyFeedGetFeedGenerator = 'app.bsky.feed.getFeedGenerator';
