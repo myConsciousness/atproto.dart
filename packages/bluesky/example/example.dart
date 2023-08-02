f238ee4aa134d8681cc2cba61aa671922adbd7c1
myConsciousness <contact@shinyakato.dev>
2023-08-02 15:19:11 +0900

    revert

--- packages/bluesky/example/example.dart
+++ packages/bluesky/example/example.dart
@@ -9,11 +9,72 @@ Future<void> main() async {
     final bluesky = bsky.Bluesky.fromSession(
       //! First you need to establish session with ATP server.
       await _session,
+
+      //! The default is `bsky.social`
+      service: 'SERVICE_NAME',
+
+      //! Automatic retry is available when server error or network error occurs
+      //! when communicating with the API.
+      retryConfig: bsky.RetryConfig(
+        maxAttempts: 5,
+        jitter: bsky.Jitter(
+          minInSeconds: 2,
+          maxInSeconds: 5,
+        ),
+        onExecute: (event) => print(
+          'Retry after ${event.intervalInSeconds} seconds...'
+          '[${event.retryCount} times]',
+        ),
+      ),
+
+      //! The default timeout is 30 seconds.
+      timeout: Duration(seconds: 20),
     );
 
-    final a = await bluesky.feeds.findTimeline();
+    //! Let's get home timeline!
+    final feeds = await bluesky.feeds.findTimeline(
+      limit: 10,
+    );
 
-    print(a.headers);
+    print(feeds);
+
+    //! Let's post cool stuff!
+    final createdRecord = await bluesky.feeds.createPost(
+      text: 'Hello, Bluesky!',
+    );
+
+    print(createdRecord);
+
+    //! And delete it.
+    await bluesky.repositories.deleteRecord(
+      uri: createdRecord.data.uri,
+    );
+
+    //! You can use Stream API easily.
+    final subscription = await bluesky.sync.subscribeRepoUpdates();
+
+    subscription.data.stream.listen((event) {
+      event.when(
+        //! You can handle commit events very easily
+        //! with RepoCommitAdaptor.
+        commit: bsky.RepoCommitAdaptor(
+          //! Create events.
+          onCreatePost: (data) => data.record,
+          onCreateLike: print,
+
+          //! Update events.
+          onUpdateProfile: print,
+
+          //! Delete events.
+          onDeletePost: print,
+        ).execute,
+        handle: print,
+        migrate: print,
+        tombstone: print,
+        info: print,
+        unknown: print,
+      );
+    });
   } on bsky.UnauthorizedException catch (e) {
     print(e);
   } on bsky.XRPCException catch (e) {
@@ -23,8 +84,9 @@ Future<void> main() async {
 
 Future<bsky.Session> get _session async {
   final session = await bsky.createSession(
-    identifier: 'shinyakato.dev', //! Like `shinyakato.bsky.social`
-    password: 'pierrot19930924ks',
+    service: 'SERVICE_NAME', //! The default is `bsky.social`
+    identifier: 'YOUR_HANDLE_OR_EMAIL', //! Like `shinyakato.bsky.social`
+    password: 'YOUR_PASSWORD',
   );
 
   return session.data;
