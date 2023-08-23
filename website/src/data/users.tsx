71af4af899152797575c602eef114591ed10eab3
myConsciousness <contact@shinyakato.dev>
2023-08-23 11:33:02 +0900

    ✨ docs: added showcase (#696)

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -11,27 +11,7 @@ import {translate} from '@docusaurus/Translate';
 import {sortBy} from '@site/src/utils/jsUtils';
 
 /*
- * ADD YOUR SITE TO THE DOCUSAURUS SHOWCASE
- *
- * Please don't submit a PR yourself: use the Github Discussion instead:
- * https://github.com/facebook/docusaurus/discussions/7826
- *
- * Instructions for maintainers:
- * - Add the site in the json array below
- * - `title` is the project's name (no need for the "Docs" suffix)
- * - A short (≤120 characters) description of the project
- * - Use relevant tags to categorize the site (read the tag descriptions on the
- *   https://docusaurus.io/showcase page and some further clarifications below)
- * - Add a local image preview (decent screenshot of the Docusaurus site)
- * - The image MUST be added to the GitHub repository, and use `require("img")`
- * - The image has to have minimum width 640 and an aspect of no wider than 2:1
- * - If a website is open-source, add a source link. The link should open
- *   to a directory containing the `docusaurus.config.js` file
- * - Resize images: node admin/scripts/resizeImage.js
- * - Run optimizt manually (see resize image script comment)
- * - Open a PR and check for reported CI errors
- *
- * Example PR: https://github.com/facebook/docusaurus/pull/7620
+ * ADD YOUR SITE TO THE atproto.dart SHOWCASE
  */
 
 // LIST OF AVAILABLE TAGS
@@ -104,6 +84,24 @@ const Users: User[] = [
     website: 'https://testflight.apple.com/join/RhoFwdem',
     source: null,
     tags: ['mobile'],
+  },
+  {
+    title: 'BlueskyDeck',
+    author: 'blueskydeck.com',
+    description: 'A multi-column web app that aims to be a TweetDeck Alternative for Bluesky. The timeline flows in real time.',
+    preview: require('./showcase/BlueskyDeck.png'),
+    website: 'https://blueskydeck.com',
+    source: null,
+    tags: ['web'],
+  },
+  {
+    title: 'SkyPurge',
+    author: 'barine.co',
+    description: 'This CLI tool allows you to delete any post in bulk. This application is a fusion of Python and bluesky_cli.',
+    preview: require('./showcase/NoImage.png'),
+    website: 'https://colab.research.google.com/drive/1XI-kQNAf5udLCWTI3qtIHEAN7wjYJOz-?usp=drive_link',
+    source: null,
+    tags: ['console'],
   },
 
   /*
