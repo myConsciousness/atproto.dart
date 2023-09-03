1df39c947d27414680105ce109ebe4c66f3b3a38
Shinya Kato <contact@shinyakato.dev>
2023-09-03 18:58:40 +0900

    added `ignore_for_file`

--- packages/bluesky/test/src/notifications/notifications_grouper_test.dart
+++ packages/bluesky/test/src/notifications/notifications_grouper_test.dart
@@ -2,6 +2,8 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
+// ignore_for_file: lines_longer_than_80_chars
+
 // 📦 Package imports:
 import 'package:test/test.dart';
 
