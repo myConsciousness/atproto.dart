bb9d9f53eff764275c71a79e90d246bd4ad55056
Shinya Kato <contact@shinyakato.dev>
2023-09-04 13:52:08 +0900

    ✨ feat: added notification utilities (#745)

--- packages/bluesky/lib/src/notifications/grouped_notification_reason.dart
+++ packages/bluesky/lib/src/notifications/grouped_notification_reason.dart
@@ -28,43 +28,43 @@ enum GroupedNotificationReason {
   const GroupedNotificationReason();
 
   /// Returns true if this reason is [like], otherwise false.
-  bool get isLike => this == GroupedNotificationReason.like;
+  bool get isLike => this == like;
 
   /// Returns true if this reason is not [like], otherwise false.
   bool get isNotLike => !isLike;
 
   /// Returns true if this reason is [customFeedLike], otherwise false.
-  bool get isCustomFeedLike => this == GroupedNotificationReason.customFeedLike;
+  bool get isCustomFeedLike => this == customFeedLike;
 
   /// Returns true if this reason is not [customFeedLike], otherwise false.
   bool get isNotCustomFeedLike => !isCustomFeedLike;
 
   /// Returns true if this reason is [repost], otherwise false.
-  bool get isRepost => this == GroupedNotificationReason.repost;
+  bool get isRepost => this == repost;
 
   /// Returns true if this reason is not [repost], otherwise false.
   bool get isNotRepost => !isRepost;
 
   /// Returns true if this reason is [follow], otherwise false.
-  bool get isFollow => this == GroupedNotificationReason.follow;
+  bool get isFollow => this == follow;
 
   /// Returns true if this reason is not [follow], otherwise false.
   bool get isNotFollow => !isFollow;
 
   /// Returns true if this reason is [mention], otherwise false.
-  bool get isMention => this == GroupedNotificationReason.mention;
+  bool get isMention => this == mention;
 
   /// Returns true if this reason is not [mention], otherwise false.
   bool get isNotMention => !isMention;
 
   /// Returns true if this reason is [reply], otherwise false.
-  bool get isReply => this == GroupedNotificationReason.reply;
+  bool get isReply => this == reply;
 
   /// Returns true if this reason is not [reply], otherwise false.
   bool get isNotReply => !isReply;
 
   /// Returns true if this reason is [quote], otherwise false.
-  bool get isQuote => this == GroupedNotificationReason.quote;
+  bool get isQuote => this == quote;
 
   /// Returns true if this reason is not [quote], otherwise false.
   bool get isNotQuote => !isQuote;
