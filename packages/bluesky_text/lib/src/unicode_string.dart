1e632b748936f376f647eb222894dcbe3d3e4ef4
Shinya Kato <contact@shinyakato.dev>
2023-09-11 17:38:54 +0900

    🔧 refactor: improved structure

--- packages/bluesky_text/lib/src/unicode_string.dart
+++ packages/bluesky_text/lib/src/unicode_string.dart
@@ -5,39 +5,8 @@
 // 🎯 Dart imports:
 import 'dart:convert';
 
-// 📦 Package imports:
-import 'package:characters/characters.dart';
-
-class UnicodeString {
-  final String utf16;
-  final List<int> utf8Int;
-  int? _graphemeLen;
-
-  UnicodeString(this.utf16) : utf8Int = utf8.encode(utf16);
-
-  int get length {
-    return utf8Int.length;
+extension UnicodeString on String {
+  int toUtf8Index(int i) {
+    return utf8.encode(substring(0, i)).length;
   }
-
-  int get graphemeLength {
-    _graphemeLen ??= utf16.characters.length;
-    return _graphemeLen!;
-  }
-
-  String slice([int? start, int? end]) {
-    return utf8Decode(utf8Int.sublist(start ?? 0, end ?? utf8Int.length));
-  }
-
-  int utf16IndexToUtf8Index(int i) {
-    return utf8.encode(utf16.substring(0, i)).length;
-  }
-
-  @override
-  String toString() {
-    return utf16;
-  }
-}
-
-String utf8Decode(List<int> bytes) {
-  return utf8.decode(bytes);
 }
