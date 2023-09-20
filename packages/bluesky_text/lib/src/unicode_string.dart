ca07aac7d1fc2f72292040572b3b18898bd93557
Shinya Kato <contact@shinyakato.dev>
2023-09-20 16:50:16 +0900

    refactor

--- packages/bluesky_text/lib/src/unicode_string.dart
+++ packages/bluesky_text/lib/src/unicode_string.dart
@@ -6,7 +6,5 @@
 import 'dart:convert';
 
 extension UnicodeString on String {
-  int toUtf8Index(int i) {
-    return utf8.encode(substring(0, i)).length;
-  }
+  int toUtf8Index(int i) => utf8.encode(substring(0, i)).length;
 }
