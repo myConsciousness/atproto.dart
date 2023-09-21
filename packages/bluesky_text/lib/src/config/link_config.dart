66fe367e78613b5a13cc28ede6de730c1ec8f188
Shinya Kato <contact@shinyakato.dev>
2023-09-21 10:15:08 +0900

    link config

---
+++ packages/bluesky_text/lib/src/config/link_config.dart
@@ -0,0 +1,11 @@
+// Copyright 2023 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+final class LinkConfig {
+  const LinkConfig({
+    this.includeProtocol = true,
+  });
+
+  final bool includeProtocol;
+}
