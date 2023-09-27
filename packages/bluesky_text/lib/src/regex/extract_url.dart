f02c072c5b94c33e131e4ca19447e818a2bf6cf5
Shinya Kato <contact@shinyakato.dev>
2023-09-27 18:34:34 +0900

    copyright

--- packages/bluesky_text/lib/src/regex/extract_url.dart
+++ packages/bluesky_text/lib/src/regex/extract_url.dart
@@ -1,3 +1,7 @@
+// Copyright 2023 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
 // 🌎 Project imports:
 import 'valid_domain.dart';
 import 'valid_port_number.dart';
