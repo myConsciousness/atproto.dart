6a30a0c4fc308068cffb0629fb588214882c8f9b
Shinya Kato <contact@shinyakato.dev>
2023-09-27 18:34:14 +0900

    revert

--- packages/bluesky_text/lib/src/bluesky_text.dart
+++ packages/bluesky_text/lib/src/bluesky_text.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 
 // 📦 Package imports:
 import 'package:characters/characters.dart';
+import 'package:icann_tlds/icann_tlds.dart';
 
 // 🌎 Project imports:
 import 'config/link_config.dart';
