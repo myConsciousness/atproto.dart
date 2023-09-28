3248c957a2e29eaafac93a6d08028483b36223bb
Shinya Kato <contact@shinyakato.dev>
2023-09-28 10:32:20 +0900

    💡 refactor: improve link detection

--- packages/bluesky_text/example/example.dart
+++ packages/bluesky_text/example/example.dart
@@ -4,54 +4,37 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'dart:convert';
+
 import 'package:bluesky/bluesky.dart' as bsky;
 import 'package:bluesky_text/bluesky_text.dart';
+import 'package:bluesky_text/src/regex/extract_url.dart';
 
 Future<void> main() async {
   //! You just need to pass text to parse.
   final text = BlueskyText(
-    'I speak 日本語 and English 🚀 @shinyakato.dev and @shinyakato.bsky.social. '
-    'Visit 🚀 https://shinyakato.dev.',
+    'テストhttps://www.google.com/search?q=deck.blue&client=safari&sca_esv=567629206&channel=iphone_bm&source=hp&ei=MMANZfeiN7P05OUP8rqmqAQ&oq=deck.blue&gs_lp=EhFtb2JpbGUtZ3dzLXdpei1ocCIJZGVjay5ibHVlMgUQABiABDIFEAAYgAQyBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB5I7hdQ3QVY3hZwA3gAkAEAmAGmAaABuwqqAQMwLjm4AQPIAQD4AQGoAgDCAgsQLhiABBjHARjRA8ICBRAuGIAEwgIHEAAYgAQYCsICDRAuGIAEGMcBGK8BGArCAgsQLhiABBjHARivAQ&sclient=mobile-gws-wiz-hpテスト２ @shinyakato.dev atprotodart.com ',
+    linkConfig: LinkConfig(
+      excludeProtocol: true,
+      maxGraphemeLength: 27,
+    ),
+  ).format();
+
+  //! And you can easily integrate with bluesky package!
+  final facets = await text.entities.toFacets();
+
+  final bluesky = bsky.Bluesky.fromSession(await _session);
+
+  await bluesky.feeds.createPost(
+    text: text.value,
+    facets: facets.map(bsky.Facet.fromJson).toList(),
   );
-
-  if (text.isLengthLimitExceeded) {
-    //! Let's split.
-    final texts = text.split();
-
-    for (final text in texts) {
-      print(text.handles);
-      print(text.links);
-      print(text.entities);
-    }
-  } else {
-    // [{type: handle, value: @shinyakato.dev, indices: {start: 35, end: 50}},
-    // {type: handle, value: @shinyakato.bsky.social, indices: {start: 55, end: 78}}]
-    print(text.handles);
-
-    // [{type: link, value: https://shinyakato.dev, indices: {start: 91, end: 113}}]
-    print(text.links);
-
-    // [{type: handle, value: @shinyakato.dev, indices: {start: 35, end: 50}},
-    // {type: handle, value: @shinyakato.bsky.social, indices: {start: 55, end: 78}},
-    // {type: link, value: https://shinyakato.dev, indices: {start: 91, end: 113}}]
-    print(text.entities);
-
-    //! And you can easily integrate with bluesky package!
-    final bluesky = bsky.Bluesky.fromSession(await _session);
-    final facets = await text.entities.toFacets();
-
-    await bluesky.feeds.createPost(
-      text: text.value,
-      facets: facets.map(bsky.Facet.fromJson).toList(),
-    );
-  }
 }
 
 Future<bsky.Session> get _session async {
   final session = await bsky.createSession(
-    service: 'SERVICE_NAME', //! The default is `bsky.social`
-    identifier: 'YOUR_HANDLE_OR_EMAIL', //! Like `shinyakato.bsky.social`
-    password: 'YOUR_PASSWORD',
+    identifier: 'shinyakato.bsky.social', //! Like `shinyakato.bsky.social`
+    password: 'rqd6-i7aq-i24m-afnm',
   );
 
   return session.data;
