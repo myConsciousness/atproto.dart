d1fc05283e34f6cc05140c572b28ef586b03c49f
Shinya Kato <contact@shinyakato.dev>
2023-10-17 20:48:50 +0900

    header

--- packages/bluesky_text/lib/src/extension/list_extension.dart
+++ packages/bluesky_text/lib/src/extension/list_extension.dart
@@ -1,3 +1,7 @@
+// Copyright 2023 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
 extension ListExtension<E> on List<E> {
   void addIfNotNull(final E? value) {
     if (value == null) return;
