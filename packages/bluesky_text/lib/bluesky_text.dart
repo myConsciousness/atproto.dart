5440a707b2d57204741e682e2e4f98b0730ffc44
Shinya Kato <contact@shinyakato.dev>
2023-10-17 15:28:35 +0900

    expose

--- packages/bluesky_text/lib/bluesky_text.dart
+++ packages/bluesky_text/lib/bluesky_text.dart
@@ -7,3 +7,4 @@ export 'package:bluesky_text/src/config/link_config.dart';
 export 'package:bluesky_text/src/entities/byte_indices.dart';
 export 'package:bluesky_text/src/entities/entities.dart';
 export 'package:bluesky_text/src/entities/entity.dart';
+export 'package:bluesky_text/src/entities/length_exceeded_entity.dart';
