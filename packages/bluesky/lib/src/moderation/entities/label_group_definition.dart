e7825a8fc1ef3a11c472140c2014fabb55139d81
Shinya Kato <contact@shinyakato.dev>
2023-12-07 17:41:51 +0900

    no need to check

--- packages/bluesky/lib/src/moderation/entities/label_group_definition.dart
+++ packages/bluesky/lib/src/moderation/entities/label_group_definition.dart
@@ -2,6 +2,7 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
+// coverage:ignore-file
 // ignore_for_file: invalid_annotation_target
 
 // 📦 Package imports:
