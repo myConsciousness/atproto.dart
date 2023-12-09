b932e5ea44f45795ddc570b060db155dcbd67669
Shinya Kato <contact@shinyakato.dev>
2023-12-09 10:20:18 +0900

    add element

--- packages/bluesky/test/src/feeds/feed_filter_test.dart
+++ packages/bluesky/test/src/feeds/feed_filter_test.dart
@@ -13,11 +13,13 @@ void main() {
     expect(FeedFilter.postsWithReplies.name, 'postsWithReplies');
     expect(FeedFilter.postsNoReplies.name, 'postsNoReplies');
     expect(FeedFilter.postsWithMedia.name, 'postsWithMedia');
+    expect(FeedFilter.postsAndAuthorThreads.name, 'postsAndAuthorThreads');
   });
 
   test('.value', () {
     expect(FeedFilter.postsWithReplies.value, 'posts_with_replies');
     expect(FeedFilter.postsNoReplies.value, 'posts_no_replies');
     expect(FeedFilter.postsWithMedia.value, 'posts_with_media');
+    expect(FeedFilter.postsAndAuthorThreads.value, 'posts_and_author_threads');
   });
 }
