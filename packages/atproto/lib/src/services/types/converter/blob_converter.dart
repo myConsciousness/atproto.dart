f12586414bcec1a86bae20325511e1b9d5ba2e57
Shinya Kato <contact@shinyakato.dev>
2024-01-01 18:46:35 +0900

    wip

--- packages/atproto/lib/src/services/types/converter/blob_converter.dart
+++ packages/atproto/lib/src/services/types/converter/blob_converter.dart
@@ -3,10 +3,11 @@
 // modification, are permitted provided the conditions.
 
 // 📦 Package imports:
+import 'package:atproto_core/atproto_core.dart' as core;
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 // 🌎 Project imports:
-import '../adaptor/blob_adaptor.dart';
+import '../../../ids.g.dart' as ids;
 import '../blob.dart';
 
 const blobConverter = _BlobConverter();
@@ -17,8 +18,22 @@ final class _BlobConverter
 
   @override
   Blob fromJson(Map<String, dynamic> json) =>
-      Blob.fromJson(toCompatibleBlob(json));
+      Blob.fromJson(_toCompatibleBlob(json));
 
   @override
   Map<String, dynamic> toJson(Blob object) => object.toJson();
+}
+
+Map<String, dynamic> _toCompatibleBlob(final Map<String, dynamic> data) {
+  final type = data[core.objectType];
+  if (type == ids.blob) return data;
+
+  return {
+    core.objectType: ids.blob,
+    'size': -1,
+    'mimeType': data['mimeType'],
+    'ref': {
+      '\$link': data['cid'],
+    }
+  };
 }
