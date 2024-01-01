60525cea3b61dd94565aab3576e2565befec3386
Shinya Kato <contact@shinyakato.dev>
2024-01-01 19:29:35 +0900

    wip

--- packages/atproto/lib/atproto.dart
+++ packages/atproto/lib/atproto.dart
@@ -2,18 +2,11 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
-export 'package:atproto/src/atproto.dart';
 export 'package:atproto/src/services/entities/account.dart';
 export 'package:atproto/src/services/entities/account_codes.dart';
 export 'package:atproto/src/services/entities/app_password.dart';
 export 'package:atproto/src/services/entities/app_passwords.dart';
 export 'package:atproto/src/services/entities/batch_action.dart';
-export 'package:atproto/src/services/entities/blob.dart';
-export 'package:atproto/src/services/entities/blob_data.dart';
-export 'package:atproto/src/services/entities/blob_ref.dart';
-export 'package:atproto/src/services/entities/blob_refs.dart';
-export 'package:atproto/src/services/entities/converter/blob_converter.dart'
-    show blobConverter;
 export 'package:atproto/src/services/entities/converter/labels_converter.dart'
     show labelsConverter;
 export 'package:atproto/src/services/entities/create_action.dart';
@@ -60,13 +53,72 @@ export 'package:atproto/src/services/entities/subscribed_repo_migrate.dart';
 export 'package:atproto/src/services/entities/subscribed_repo_tombstone.dart';
 export 'package:atproto/src/services/entities/update_action.dart';
 export 'package:atproto/src/services/entities/signing_key.dart';
-export 'package:atproto/src/services/identity_service.dart';
 export 'package:atproto/src/services/constants/moderation_reason_type.dart';
+
+export 'package:atproto/src/atproto.dart';
+export 'package:atproto/src/services/identity_service.dart';
 export 'package:atproto/src/services/moderation_service.dart';
 export 'package:atproto/src/services/repo_service.dart';
 export 'package:atproto/src/services/server_service.dart';
 export 'package:atproto/src/services/sync_service.dart';
 export 'package:atproto/src/services/label_service.dart';
+export 'package:atproto/src/services/types/converter/blob_converter.dart'
+    show blobConverter;
+export 'package:atproto/src/services/types/admin_defs_repo_ref.dart';
+export 'package:atproto/src/services/types/blob.dart';
+export 'package:atproto/src/services/types/blob_data.dart';
+export 'package:atproto/src/services/types/blob_ref.dart';
+export 'package:atproto/src/services/types/blob_refs.dart';
+export 'package:atproto/src/services/types/identity_resolve_handle.dart';
+export 'package:atproto/src/services/types/label_defs_label.dart';
+export 'package:atproto/src/services/types/label_defs_self_label.dart';
+export 'package:atproto/src/services/types/label_defs_self_labels.dart';
+export 'package:atproto/src/services/types/label_query_labels.dart';
+export 'package:atproto/src/services/types/label_subscribe_labels_info.dart';
+export 'package:atproto/src/services/types/label_subscribe_labels_labels.dart';
+export 'package:atproto/src/services/types/label_subscribe_labels_refs.dart';
+export 'package:atproto/src/services/types/moderation_create_report.dart';
+export 'package:atproto/src/services/types/moderation_create_report_subject_refs.dart';
+export 'package:atproto/src/services/types/moderation_defs_reason_type.dart';
+export 'package:atproto/src/services/types/repo_apply_writes_create.dart';
+export 'package:atproto/src/services/types/repo_apply_writes_delete.dart';
+export 'package:atproto/src/services/types/repo_apply_writes_update.dart';
+export 'package:atproto/src/services/types/repo_apply_writes_writes.dart';
+export 'package:atproto/src/services/types/repo_describe_repo.dart';
+export 'package:atproto/src/services/types/repo_list_records.dart';
+export 'package:atproto/src/services/types/repo_list_records_record.dart';
+export 'package:atproto/src/services/types/repo_strong_ref.dart';
+export 'package:atproto/src/services/types/server_create_account.dart';
+export 'package:atproto/src/services/types/server_create_app_password_app_password.dart';
+export 'package:atproto/src/services/types/server_create_invite_code.dart';
+export 'package:atproto/src/services/types/server_create_invite_codes.dart';
+export 'package:atproto/src/services/types/server_create_invite_codes_account_codes.dart';
+export 'package:atproto/src/services/types/server_defs_invite_code.dart';
+export 'package:atproto/src/services/types/server_defs_invite_code_use.dart';
+export 'package:atproto/src/services/types/server_describe_server.dart';
+export 'package:atproto/src/services/types/server_describe_server_links.dart';
+export 'package:atproto/src/services/types/server_get_account_invite_codes.dart';
+export 'package:atproto/src/services/types/server_get_session.dart';
+export 'package:atproto/src/services/types/server_list_app_passwords.dart';
+export 'package:atproto/src/services/types/server_list_app_passwords_app_password.dart';
+export 'package:atproto/src/services/types/server_request_email_update.dart';
+export 'package:atproto/src/services/types/server_reserve_signing_key.dart';
+export 'package:atproto/src/services/types/sync_get_blocks.dart';
+export 'package:atproto/src/services/types/sync_get_blocks_block.dart';
+export 'package:atproto/src/services/types/sync_get_latest_commit.dart';
+export 'package:atproto/src/services/types/sync_get_repo.dart';
+export 'package:atproto/src/services/types/sync_get_repo_commit.dart';
+export 'package:atproto/src/services/types/sync_list_repos.dart';
+export 'package:atproto/src/services/types/sync_list_repos_repo.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_commit.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_handle.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_info.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_migrate.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_refs.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_repo_op.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_repo_op_action.dart';
+export 'package:atproto/src/services/types/sync_subscribe_repos_tombstone.dart';
+
 export 'package:atproto_core/atproto_core.dart'
     show
         XRPCResponse,
