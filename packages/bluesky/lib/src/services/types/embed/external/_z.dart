ff7ba61e5a8d650ea5211655e053c908304f330b
Shinya Kato <contact@shinyakato.dev>
2024-01-21 15:09:29 +0900

    wip

--- packages/bluesky/lib/src/services/types/embed/external/_z.dart
+++ packages/bluesky/lib/src/services/types/embed/external/_z.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
 export 'external.dart';
 export 'main.dart';
 export 'view_external.dart';
