dc17dd42adf3909a7829c4d0ccc8caaa5254e560
Shinya Kato <contact@shinyakato.dev>
2024-01-21 15:03:42 +0900

    wip

--- packages/bluesky/lib/src/services/types/embed/record/unions/view_record.dart
+++ packages/bluesky/lib/src/services/types/embed/record/unions/view_record.dart
@@ -1,4 +1,4 @@
-/// Copyright 2023 Shinya Kato. All rights reserved.
+/// Copyright 2024 Shinya Kato. All rights reserved.
 /// Redistribution and use in source and binary forms, with or without
 /// modification, are permitted provided the conditions.
 
