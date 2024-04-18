2c371b280e06c44831517a22c0a30c125528e839
Shinya Kato <contact@shinyakato.dev>
2024-04-18 18:30:08 +0900

    moderation ui

---
+++ packages/bluesky/lib/src/moderation/types/ui.dart
@@ -0,0 +1,23 @@
+// Copyright 2024 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+// 📦 Package imports:
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+// 🌎 Project imports:
+import 'behaviors/moderation_cause.dart';
+import 'mod_object.dart';
+
+part 'ui.freezed.dart';
+
+@modObject
+class ModerationUI with _$ModerationUI {
+  const factory ModerationUI({
+    @Default(false) bool noOverride,
+    @Default([]) ModerationCause filters,
+    @Default([]) ModerationCause blurs,
+    @Default([]) ModerationCause alerts,
+    @Default([]) ModerationCause informs,
+  }) = _ModerationUI;
+}
---
+++ packages/bluesky/lib/src/moderation/types/ui.freezed.dart
@@ -0,0 +1,269 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'ui.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$ModerationUI {
+  bool get noOverride => throw _privateConstructorUsedError;
+  ModerationCause get filters => throw _privateConstructorUsedError;
+  ModerationCause get blurs => throw _privateConstructorUsedError;
+  ModerationCause get alerts => throw _privateConstructorUsedError;
+  ModerationCause get informs => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ModerationUICopyWith<ModerationUI> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ModerationUICopyWith<$Res> {
+  factory $ModerationUICopyWith(
+          ModerationUI value, $Res Function(ModerationUI) then) =
+      _$ModerationUICopyWithImpl<$Res, ModerationUI>;
+  @useResult
+  $Res call(
+      {bool noOverride,
+      ModerationCause filters,
+      ModerationCause blurs,
+      ModerationCause alerts,
+      ModerationCause informs});
+
+  $ModerationCauseCopyWith<$Res> get filters;
+  $ModerationCauseCopyWith<$Res> get blurs;
+  $ModerationCauseCopyWith<$Res> get alerts;
+  $ModerationCauseCopyWith<$Res> get informs;
+}
+
+/// @nodoc
+class _$ModerationUICopyWithImpl<$Res, $Val extends ModerationUI>
+    implements $ModerationUICopyWith<$Res> {
+  _$ModerationUICopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? noOverride = null,
+    Object? filters = null,
+    Object? blurs = null,
+    Object? alerts = null,
+    Object? informs = null,
+  }) {
+    return _then(_value.copyWith(
+      noOverride: null == noOverride
+          ? _value.noOverride
+          : noOverride // ignore: cast_nullable_to_non_nullable
+              as bool,
+      filters: null == filters
+          ? _value.filters
+          : filters // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+      blurs: null == blurs
+          ? _value.blurs
+          : blurs // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+      alerts: null == alerts
+          ? _value.alerts
+          : alerts // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+      informs: null == informs
+          ? _value.informs
+          : informs // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $ModerationCauseCopyWith<$Res> get filters {
+    return $ModerationCauseCopyWith<$Res>(_value.filters, (value) {
+      return _then(_value.copyWith(filters: value) as $Val);
+    });
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $ModerationCauseCopyWith<$Res> get blurs {
+    return $ModerationCauseCopyWith<$Res>(_value.blurs, (value) {
+      return _then(_value.copyWith(blurs: value) as $Val);
+    });
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $ModerationCauseCopyWith<$Res> get alerts {
+    return $ModerationCauseCopyWith<$Res>(_value.alerts, (value) {
+      return _then(_value.copyWith(alerts: value) as $Val);
+    });
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $ModerationCauseCopyWith<$Res> get informs {
+    return $ModerationCauseCopyWith<$Res>(_value.informs, (value) {
+      return _then(_value.copyWith(informs: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$ModerationUIImplCopyWith<$Res>
+    implements $ModerationUICopyWith<$Res> {
+  factory _$$ModerationUIImplCopyWith(
+          _$ModerationUIImpl value, $Res Function(_$ModerationUIImpl) then) =
+      __$$ModerationUIImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {bool noOverride,
+      ModerationCause filters,
+      ModerationCause blurs,
+      ModerationCause alerts,
+      ModerationCause informs});
+
+  @override
+  $ModerationCauseCopyWith<$Res> get filters;
+  @override
+  $ModerationCauseCopyWith<$Res> get blurs;
+  @override
+  $ModerationCauseCopyWith<$Res> get alerts;
+  @override
+  $ModerationCauseCopyWith<$Res> get informs;
+}
+
+/// @nodoc
+class __$$ModerationUIImplCopyWithImpl<$Res>
+    extends _$ModerationUICopyWithImpl<$Res, _$ModerationUIImpl>
+    implements _$$ModerationUIImplCopyWith<$Res> {
+  __$$ModerationUIImplCopyWithImpl(
+      _$ModerationUIImpl _value, $Res Function(_$ModerationUIImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? noOverride = null,
+    Object? filters = null,
+    Object? blurs = null,
+    Object? alerts = null,
+    Object? informs = null,
+  }) {
+    return _then(_$ModerationUIImpl(
+      noOverride: null == noOverride
+          ? _value.noOverride
+          : noOverride // ignore: cast_nullable_to_non_nullable
+              as bool,
+      filters: null == filters
+          ? _value.filters
+          : filters // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+      blurs: null == blurs
+          ? _value.blurs
+          : blurs // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+      alerts: null == alerts
+          ? _value.alerts
+          : alerts // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+      informs: null == informs
+          ? _value.informs
+          : informs // ignore: cast_nullable_to_non_nullable
+              as ModerationCause,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$ModerationUIImpl implements _ModerationUI {
+  const _$ModerationUIImpl(
+      {this.noOverride = false,
+      this.filters = const [],
+      this.blurs = const [],
+      this.alerts = const [],
+      this.informs = const []});
+
+  @override
+  @JsonKey()
+  final bool noOverride;
+  @override
+  @JsonKey()
+  final ModerationCause filters;
+  @override
+  @JsonKey()
+  final ModerationCause blurs;
+  @override
+  @JsonKey()
+  final ModerationCause alerts;
+  @override
+  @JsonKey()
+  final ModerationCause informs;
+
+  @override
+  String toString() {
+    return 'ModerationUI(noOverride: $noOverride, filters: $filters, blurs: $blurs, alerts: $alerts, informs: $informs)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ModerationUIImpl &&
+            (identical(other.noOverride, noOverride) ||
+                other.noOverride == noOverride) &&
+            (identical(other.filters, filters) || other.filters == filters) &&
+            (identical(other.blurs, blurs) || other.blurs == blurs) &&
+            (identical(other.alerts, alerts) || other.alerts == alerts) &&
+            (identical(other.informs, informs) || other.informs == informs));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, noOverride, filters, blurs, alerts, informs);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ModerationUIImplCopyWith<_$ModerationUIImpl> get copyWith =>
+      __$$ModerationUIImplCopyWithImpl<_$ModerationUIImpl>(this, _$identity);
+}
+
+abstract class _ModerationUI implements ModerationUI {
+  const factory _ModerationUI(
+      {final bool noOverride,
+      final ModerationCause filters,
+      final ModerationCause blurs,
+      final ModerationCause alerts,
+      final ModerationCause informs}) = _$ModerationUIImpl;
+
+  @override
+  bool get noOverride;
+  @override
+  ModerationCause get filters;
+  @override
+  ModerationCause get blurs;
+  @override
+  ModerationCause get alerts;
+  @override
+  ModerationCause get informs;
+  @override
+  @JsonKey(ignore: true)
+  _$$ModerationUIImplCopyWith<_$ModerationUIImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
