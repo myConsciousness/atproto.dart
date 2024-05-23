2aea62839d7cb5312a127614a07e95ae1d9a904d
Shinya Kato <contact@shinyakato.dev>
2024-05-23 16:30:25 +0900

    wip

--- packages/bluesky/test/src/moderation/suite/utils/result_flag.dart
+++ packages/bluesky/test/src/moderation/suite/utils/result_flag.dart
@@ -8,4 +8,16 @@ enum ModerationTestSuiteResultFlag {
   alert,
   inform,
   noOverride;
+
+  const ModerationTestSuiteResultFlag();
+
+  static ModerationTestSuiteResultFlag valueOf(final String value) {
+    for (final $value in values) {
+      if ($value.name == value) {
+        return $value;
+      }
+    }
+
+    throw Error();
+  }
 }
