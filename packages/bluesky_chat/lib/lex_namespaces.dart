f0e35cd2e46412273ad83aea90b2e4a27fed20ed
Shinya Kato <contact@shinyakato.dev>
2024-07-01 14:51:55 +0900

    remove

--- packages/bluesky_chat/lib/lex_namespaces.dart
+++
@@ -1,5 +0,0 @@
-// Copyright 2024 Shinya Kato. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
-
-export 'package:bluesky_chat/src/nsids.g.dart';
