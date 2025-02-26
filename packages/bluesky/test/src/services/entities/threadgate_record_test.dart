cd6117b50623b3e7ce988b6b879eb14b4297325a
Shinya <github.sixfold374@passinbox.com>
2025-02-26 14:34:41 +0900

    fmt
    Co-Authored-By: Sébastien Gruhier <3385+sgruhier@users.noreply.github.com>

--- packages/bluesky/test/src/services/entities/threadgate_record_test.dart
+++ packages/bluesky/test/src/services/entities/threadgate_record_test.dart
@@ -1,15 +1,19 @@
 // Package imports:
+
+// Package imports:
+import 'package:test/test.dart';
+
 // Project imports:
 import 'package:bluesky/src/services/entities/thread_rule.dart';
 import 'package:bluesky/src/services/entities/threadgate_record.dart';
-import 'package:test/test.dart';
 
 void main() {
   group('ThreadgateRecord', () {
     test('fromJson', () {
       final json = {
         'threadgate': {
-          'uri': 'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.feed.threadgate/3lijbupcbhs2c',
+          'uri':
+              'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.feed.threadgate/3lijbupcbhs2c',
           'cid': 'bafyreibjxilvh6izvp3kxgxpcgiipyzckxav7ffvafhrom5m2chx4vab3u',
           'record': {
             '\$type': 'app.bsky.feed.threadgate',
@@ -19,28 +23,33 @@ void main() {
               {'\$type': 'app.bsky.feed.threadgate#followerRule'},
               {
                 '\$type': 'app.bsky.feed.threadgate#listRule',
-                'list': 'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.graph.list/3lhybxukqos2n'
+                'list':
+                    'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.graph.list/3lhybxukqos2n'
               },
               {
                 '\$type': 'app.bsky.feed.threadgate#listRule',
-                'list': 'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.graph.list/3lgae7jyaf62n'
+                'list':
+                    'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.graph.list/3lgae7jyaf62n'
               }
             ],
             'createdAt': '2025-02-19T07:46:42.122Z',
             'hiddenReplies': [],
-            'post': 'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.feed.post/3lijbupcbhs2c'
+            'post':
+                'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.feed.post/3lijbupcbhs2c'
           },
           'lists': []
         }
       };
 
       // ignore: lines_longer_than_80_chars
-      final record = ThreadgateRecord.fromJson(json['threadgate']!['record'] as Map<String, Object?>);
+      final record = ThreadgateRecord.fromJson(
+          json['threadgate']!['record'] as Map<String, Object?>);
 
       expect(record.type, equals('app.bsky.feed.threadgate'));
       expect(
         record.postUri.toString(),
-        equals('at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.feed.post/3lijbupcbhs2c'),
+        equals(
+            'at://did:plc:irshngrijywpgtvdsdkfmb34/app.bsky.feed.post/3lijbupcbhs2c'),
       );
       expect(record.allowRules, hasLength(5));
       expect(record.allowRules![0], isA<UThreadRuleMention>());
