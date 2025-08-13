d29de2b1511b2042b833fcefca5aeed71dd4f56c
Shinya <github.sixfold374@passinbox.com>
2025-08-13 15:48:53 +0900

    chore: fix header

--- bin/shared/config_loader.dart
+++ bin/shared/config_loader.dart
@@ -1,5 +1,5 @@
-// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
+// Copyright (c) 2023-2025, Shinya Kato.
+// All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:io';
