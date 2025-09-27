4f7a951230d734ca81f243f9fb9d0146f874aeec
Shinya <github.sixfold374@passinbox.com>
2025-09-27 20:23:27 +0900

    fix(did_plc): update copyright notice to 2023-2025 format
    - Update mock_plc_directory.dart copyright header to match project standard
    - Ensure consistent copyright format across all did_plc files

--- packages/did_plc/test/mocks/mock_plc_directory.dart
+++ packages/did_plc/test/mocks/mock_plc_directory.dart
@@ -1,6 +1,6 @@
-// Copyright 2024 Shinya Kato. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
+// Copyright (c) 2023-2025, Shinya Kato.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
 import 'dart:math';
