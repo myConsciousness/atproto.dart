0e479130557e590ff9e4f914cd9a40bde5fb6afa
Roscoe Rubin-Rottenberg <roscoe@knotbin.com>
2026-03-27 16:52:56 -0700

    focus in test

--- packages/atproto_core/test/src/clients/service_context_test.dart
+++ packages/atproto_core/test/src/clients/service_context_test.dart
@@ -1,4 +1,16 @@
+// ignore_for_file: depend_on_referenced_packages
+
+// Dart imports:
+import 'dart:convert';
+
 // Package imports:
+import 'package:at_primitives/nsid.dart';
+import 'package:atproto_oauth/atproto_oauth.dart';
+import 'package:atproto_oauth/src/helper/helper.dart' show getKeyPair;
+import 'package:atproto_oauth/src/helper/private_key.dart'
+    show encodePrivateKey;
+import 'package:atproto_oauth/src/helper/public_key.dart' show encodePublicKey;
+import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
 
 // Project imports:
@@ -153,4 +165,71 @@ void main() {
       expect(context.service, 'bsky.app');
     });
   });
+
+  group('.get', () {
+    test(
+      'uses stored client id for DPoP headers when tokens omit client_id',
+      () async {
+        final keyPair = getKeyPair();
+        final publicKey = encodePublicKey(keyPair.publicKey as dynamic);
+        final privateKey = encodePrivateKey(keyPair.privateKey as dynamic);
+        Map<String, String>? requestHeaders;
+
+        final context = ServiceContext(
+          oAuthSession: OAuthSession(
+            accessToken: _jwt({
+              'sub': 'did:plc:testaccount',
+              'scope': 'atproto transition:generic',
+              'iss': 'https://auth.sprk.so',
+              'aud': 'did:web:pds.sprk.so',
+              'exp': 1893456000,
+              'iat': 1893452400,
+            }),
+            refreshToken: _jwt({
+              'sub': 'did:plc:testaccount',
+              'exp': 1893542400,
+              'iat': 1893452400,
+            }),
+            tokenType: 'DPoP',
+            scope: 'atproto transition:generic',
+            expiresAt: DateTime.utc(2030),
+            sub: 'did:plc:testaccount',
+            $clientId: 'https://sprk.so/oauth-client-metadata.json',
+            $dPoPNonce: 'nonce',
+            $publicKey: publicKey,
+            $privateKey: privateKey,
+          ),
+          getClient: (url, {headers}) async {
+            requestHeaders = headers;
+
+            return http.Response(
+              '{}',
+              200,
+              headers: {'content-type': 'application/json'},
+              request: http.Request('GET', url),
+            );
+          },
+        );
+
+        await context.get<Map<String, Object?>>(
+          NSID.create('server.atproto.com', 'describeServer'),
+          to: (json) => json,
+        );
+
+        expect(
+          requestHeaders?['Authorization'],
+          'DPoP ${context.oAuthSession!.accessToken}',
+        );
+        expect(requestHeaders?['DPoP'], isNotEmpty);
+      },
+    );
+  });
+}
+
+String _jwt(Map<String, Object?> payload) {
+  final encodedPayload = base64Url
+      .encode(utf8.encode(jsonEncode(payload)))
+      .replaceAll('=', '');
+
+  return 'header.$encodedPayload.signature';
 }
